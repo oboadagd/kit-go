f33c98b4cba4c55acfa339f4250e97db3f3db6de
oboadagd <oscar.boada@shipt.com>
2022-08-31 17:42:08 -0500

    first commit

---
+++ redis/example1/example1.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	redisKit "github.com/laironacosta/kit-go/redis"
+	log "github.com/sirupsen/logrus"
+	"time"
+)
+
+type RedisClient struct {
+	db *redis.Client
+}
+
+func main() {
+	client := NewRedisClient()
+	defer client.db.Close()
+
+	ctx := context.Background()
+
+	key := "key1"
+	value := "val1"
+
+	client.SetKey(ctx, key, value, 10*time.Second)
+	val := client.GetKey(ctx, key)
+
+	log.Infof("Key: %s, Value: %s", key, val)
+}
+
+func NewRedisClient() *RedisClient {
+	return &RedisClient{
+		db: redisKit.NewRedisDB(&redis.Options{
+			Addr:     "redis-test:6379",
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		}),
+	}
+}
+
+func (r *RedisClient) SetKey(ctx context.Context, key string, val string, expiration time.Duration) {
+	if err := r.db.Set(ctx, key, val, expiration).Err(); err != nil {
+		log.Panicf("Redis error [%+v] in set func", err)
+	}
+}
+
+func (r *RedisClient) GetKey(ctx context.Context, key string) string {
+	val, err := r.db.Get(ctx, key).Result()
+	if err != nil {
+		log.Panicf("Redis error [%+v] in get func", err)
+	}
+
+	return val
+}
